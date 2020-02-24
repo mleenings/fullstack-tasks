76885083274150c19cf8bcefa85c9fa0df4f2303
mleenings <mleenings@web.de>
2020-02-24 17:50:11 +0100

    adapt 01-build.sh with clean

--- 01-build.sh
+++ 01-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-(cd tasks-backend && gradle build)
+(cd tasks-backend && gradle clean build)
 # (cd tasks-ui && eslint . --fix)
-(cd tasks-ui && gradle build)
+(cd tasks-ui && gradle clean build)
 # or simpler:
 # gradle build
